e592f369974b961329c7aaf5da650f0fc5bfe7d6
DivineMatongo <dstanley3247@gmail.com>
2026-04-12 01:00:01 +0200

    Change addNumber function to addText
    - New nomenclature better captures the expanded intent of the function,
    as it will also be called to add non-digits like the decimal point.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const screenLine1 = document.querySelector("#line1");
 const screenLine2 = document.querySelector("#line2");
 
-function appendNumber(number) {
-    screenLine2.textContent += number;
+function appendToScreen(text) {
+    screenLine2.textContent += text;
 }
