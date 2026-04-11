82548c6b4094a458672eff52c4492a54b5820d29
DivineMatongo <dstanley3247@gmail.com>
2026-04-12 01:18:40 +0200

    Add function for adding decimal point to screen

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const screenLine2 = document.querySelector("#line2");
 function appendToScreen(text) {
     screenLine2.textContent += text;
 }
+
+function addPeriod() {
+    if (!screenLine2.textContent.includes(".")) {
+        appendToScreen(".");
+    }
+}
